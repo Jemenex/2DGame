302917281526ef1ae3bebce03d873863f7de3704
Jemenex <Jemenex@gmail.com>
2021-12-16 18:45:08 -0500

    Update main.go
    Repositioning the images and adding the bottom box. Cleaning up unused code.

--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	_ "image/png" //image functions import
 
-	//"image/color" //needed for color fill
+	"image/color" //needed for color fill
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil" //needed for Hello World screen print
 )
 
+//Vars
+var windowWidth = 1000
+var windowHeight = 800
+
+//Images
+var divider *ebiten.Image
 var img *ebiten.Image //variable declared for pointer image
 
 func init() { //init function grabbing image from directory
@@ -29,35 +35,37 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Display the information with "X: xx, Y: xx" format
+	// Capture Mouse Position in variables
 	x, y := ebiten.CursorPosition()
+	// Display Mouse Position
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("X: %d, Y: %d", x, y))
+
 	npc := &ebiten.DrawImageOptions{}
 	npc.ColorM.ChangeHSV(2.85, 2.00, 1.00)
 	npc.GeoM.Scale(-1, 1)
-	npc.GeoM.Translate(615, 30)
+	npc.GeoM.Translate(950, 80)
 
 	char := &ebiten.DrawImageOptions{}
-	char.GeoM.Translate(25, 30)
+	char.GeoM.Translate(50, 80)
+
+	div := &ebiten.DrawImageOptions{}
+	div.GeoM.Translate(float64(windowWidth)/20.00, float64(windowHeight)/2.00)
 
 	screen.DrawImage(img, char)
-	screen.DrawImage(img, npc) //Draw same Image with declareed changes 2nd argument (scaled 1.5 translated 50,50 = op)
-	//screen.DrawImage(img, nil) //Draw Default Image on Screen every tick
-	//screen.Fill(color.RGBA{0xff, 0, 0, 0xff}) //fills screen color Red
-	//ebitenutil.DebugPrint(screen, "Hello, World!") //Draws Hello World Text
+	screen.DrawImage(img, npc)
+	// Create an bottom box
+	divider = ebiten.NewImage(int(float64(windowWidth)*.9), int(float64(windowHeight)*.45))
+	divider.Fill(color.RGBA{0xb0, 0xb0, 0xb0, 0x5f})
+	screen.DrawImage(divider, div)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 640, 480 //sets window size
-	//return 320, 240 // sets window size
+	return windowWidth, windowHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Geometry Matrix") //sets Window Title to "Geometry Matrix"
-	//ebiten.SetWindowTitle("Render an image") //sets Window Title to "Render an image"
-	//ebiten.SetWindowTitle("Fill") //sets Window Title to Fill
-	//ebiten.SetWindowTitle("Hello, World!") //sets Window Title to Hello World!
+	ebiten.SetWindowSize(windowWidth, windowHeight)
+	ebiten.SetWindowTitle("2D Game")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
