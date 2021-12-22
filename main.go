079202ff12f235aa152ccaeba6ac5cb4faa5b2c2
Jemenex <Jemenex@gmail.com>
2021-12-22 13:15:20 -0500

    Update main.go
    Spell/Action options text added.
    Spell/Action text positioned.
    Simple Pointer added.
    Simple Pointer made Interactive to show selection with arrow keys

--- main.go
+++ main.go
@@ -7,16 +7,24 @@ import (
 
 	"log"
 
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil" //needed for Hello World screen print
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/text"
 )
 
 var windowWidth, windowHeight int = 1280, 960
 
 var spell1 = Action{"Basic Attack", 2, 0}
 var spell2 = Action{"Heavy Attack", 4, 2}
-var spell3 = Action{"Bite", 1, 0}
-var spell4 = Action{"Scratch", 2, 2}
+var spell3 = Action{"Drink Potion", 4, 2}
+var spell4 = Action{"Heavy Attack", 4, 2}
+var spell5 = Action{"Bite", 1, 0}
+var spell6 = Action{"Scratch", 2, 2}
 
 var player Entity
 var enemy Entity
@@ -29,11 +37,20 @@ var PlayerDead bool = false
 var EnemyDead bool = false
 
 var box *ebiten.Image
+var arrow *ebiten.Image
 var img *ebiten.Image //variable declared for pointer image
+
+var arrowPos = [2]int{11, 12}
+var (
+	mplusNormalFont font.Face
+)
 
 func init() { //init function grabbing image from directory
 	var err error
 	img, _, err = ebitenutil.NewImageFromFile("gopher.png")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	player.Name = "Blue Gopher"
 	player.Actions = [2]Action{spell1, spell2}
@@ -44,7 +61,7 @@ func init() { //init function grabbing image from directory
 	player.Position = [2]int{windowWidth * 1 / 20, windowHeight * 2 / 10}
 
 	enemy.Name = "Red Gopher"
-	enemy.Actions = [2]Action{spell3, spell4}
+	enemy.Actions = [2]Action{spell5, spell6}
 	enemy.Health = [2]int{10, 10}
 	enemy.Image = *img
 	enemy.Size = [2]int{x, y}
@@ -60,22 +77,33 @@ func init() { //init function grabbing image from directory
 	fmt.Println(enemy.Size)
 	fmt.Println(img.Bounds())
 
+	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	const dpi = 72
+	mplusNormalFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    24,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 type Action struct {
-	Name     string
-	Damage   int
-	CoolDown int
+	Name           string
+	HealthModifier int //How much it modifies health + or -
+	CoolDown       int
 }
 
 type Entity struct {
 	Name     string
 	Position [2]int
 	Size     [2]int
-	Health   [2]int
+	Health   [2]int //{Current Health, Total Health}
 	Actions  [2]Action
 	Image    ebiten.Image
 }
@@ -84,17 +112,28 @@ type Game struct {
 }
 
 func (g *Game) Update() error {
-	if ebiten.IsKeyPressed(ebiten.KeyUp) && player.Health[0] > 1 {
+	/*if inpututil.IsKeyJustPressed(ebiten.KeyUp) && player.Health[0] > 1 {
 		player.Health[0] -= 1
-	} else if ebiten.IsKeyPressed(ebiten.KeyUp) && player.Health[0] == 1 {
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyUp) && player.Health[0] == 1 {
 		PlayerDead = true
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyDown) && enemy.Health[0] > 1 {
+	if inpututil.IsKeyJustPressed(ebiten.KeyDown) && enemy.Health[0] > 1 {
 		enemy.Health[0] -= 1
-	} else if ebiten.IsKeyPressed(ebiten.KeyDown) && enemy.Health[0] == 1 {
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyDown) && enemy.Health[0] == 1 {
 		EnemyDead = true
+	}*/
+	if inpututil.IsKeyJustPressed(ebiten.KeyUp) {
+		arrowPos[1] = 0
 	}
-	//fmt.Println(player.Health)
+	if inpututil.IsKeyJustPressed(ebiten.KeyDown) {
+		arrowPos[1] = 12
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyLeft) {
+		arrowPos[0] = 0
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyRight) {
+		arrowPos[0] = 11
+	}
 	return nil
 }
 
@@ -116,10 +155,13 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	char.GeoM.Translate(float64(player.Position[0]), float64(player.Position[1]))
 
 	b := &ebiten.DrawImageOptions{}
-	b.GeoM.Translate(float64(windowWidth)*2/20, float64(windowHeight)*15/20)
-	box = ebiten.NewImage(windowWidth*16/20, windowHeight*3/20)
-	box.Fill(color.RGBA{0xb0, 0xb0, 0xb0, 0x2f})
-	//box = ebiten.NewImage(50, 50)
+	b.GeoM.Translate(float64(windowWidth)*4/20, float64(windowHeight)*13/20)
+	box = ebiten.NewImage(windowWidth*12/20, windowHeight*6/20)
+	box.Fill(color.RGBA{0xb0, 0xb0, 0xb0, 0x0f})
+	arr := &ebiten.DrawImageOptions{}
+	arr.GeoM.Translate(float64(windowWidth*(11+arrowPos[0])/40), float64(windowHeight*(57+arrowPos[1])/80))
+	arrow = ebiten.NewImage(12, 12)
+	arrow.Fill(color.White)
 
 	hpEnemy := &ebiten.DrawImageOptions{}
 	hpEnemy.GeoM.Translate(float64(enemy.Position[0]-enemy.Size[0]*3/2+30), float64(enemy.Position[1]-40))
@@ -127,7 +169,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	hpPlayer.GeoM.Translate(float64(player.Position[0]+30), float64(player.Position[1]-40))
 	healthBarGreenP = ebiten.NewImage(300*player.Health[0]/player.Health[1], 30)
 	healthBarGreenP.Fill(color.RGBA{0x00, 0xff, 0x00, 0xff})
-	fmt.Println(player.Health)
+	//fmt.Println(player.Health)
 	healthBarGreenE = ebiten.NewImage(300*enemy.Health[0]/enemy.Health[1], 30)
 	healthBarGreenE.Fill(color.RGBA{0x00, 0xff, 0x00, 0xff})
 
@@ -143,8 +185,14 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		screen.DrawImage(healthBarGreenP, hpPlayer)
 	}
 	screen.DrawImage(box, b)
+	screen.DrawImage(arrow, arr)
 	screen.DrawImage(img, char)
 	screen.DrawImage(img, npc)
+	fmt.Println(b.GeoM)
+	text.Draw(screen, "first spell", mplusNormalFont, windowWidth*12/40, windowHeight*29/40, color.White)
+	text.Draw(screen, "second spell", mplusNormalFont, windowWidth*(12+11)/40, windowHeight*29/40, color.White)
+	text.Draw(screen, "third spell", mplusNormalFont, windowWidth*12/40, windowHeight*(29+6)/40, color.White)
+	text.Draw(screen, "fourth spell", mplusNormalFont, windowWidth*(12+11)/40, windowHeight*(29+6)/40, color.White)
 
 }
 
@@ -161,3 +209,14 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+/*
+SPELLS OPTIONS AND SELECTION
+	DRAW TEXT -DONE
+	POSITION TEXT - DONE
+	DRAW SELECTOR - DONE
+	ATTACH VARIABLE WITH VALUE FOR POSITION  0,0  0,1 1,0 1,1 -DONE
+	ATTACH VARIABLE CHANGE TO KEY PRESS DIRECTIONALLY - DONE
+ENEMY TURN
+BATTLE LOOP
+*/
