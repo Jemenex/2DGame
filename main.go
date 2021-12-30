c5dd145fbe9b79645032cc5613f9a7ba178e4aa5
Jemenex <Jemenex@gmail.com>
2021-12-30 12:34:33 -0500

    Update main.go
    Added working spell buff effects.
    Added working spell cool downs.
    Added working spell buff removal after cooldowns.
    Changed placeholder spell bar names.
    Added working Turn counter.

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"image/color"
 	_ "image/png" //image functions import
+	"math/rand"
+	"time"
 
 	"log"
 
@@ -19,32 +21,36 @@ import (
 
 var windowWidth, windowHeight int = 1280, 960
 
-var spell1 = Action{"Basic Attack", 2, 0, "damage"}
-var spell2 = Action{"Heavy Attack", 4, 2, "damage"}
-var spell3 = Action{"Drink Potion", 6, 4, "heal"}
-var spell4 = Action{"Defense Up", 1, 4, "buff"}
-var spell5 = Action{"Bite", 1, 0, "damage"}
-var spell6 = Action{"Scratch", 2, 2, "damage"}
-var spell7 = Action{"Enrage", 1, 4, "buff"}
-var spell8 = Action{"Block", 1, 3, "buff"}
+var spell1 = Action{"Basic Attack", 2, [2]int{0, 0}, "damage"}
+var spell2 = Action{"Heavy Attack", 4, [2]int{1, 0}, "damage"}
+var spell3 = Action{"Drink Potion", 3, [2]int{2, 0}, "heal"}
+var spell4 = Action{"Attack Up", 1, [2]int{2, 0}, "attackBuff"}
+var spell5 = Action{"Bite", 1, [2]int{0, 0}, "damage"}
+var spell6 = Action{"Scratch", 2, [2]int{1, 0}, "damage"}
+var spell7 = Action{"Enrage", 1, [2]int{2, 0}, "attackBuff"}
+var spell8 = Action{"Block", 1, [2]int{2, 0}, "defenseBuff"}
+
+var DurationOfTime = time.Duration(3) * time.Second
 
 var player Entity
 var enemy Entity
 
 var turnText string = "Battle Starts!"
+var turn int = 0
 
 var healthBarGreenE *ebiten.Image
 var healthBarGreenP *ebiten.Image
 var healthBarRed *ebiten.Image
 
-var PlayerDead bool = false
-var EnemyDead bool = false
+var playerTurn bool = true
+var playerDead bool = false
+var enemyDead bool = false
 
 var box *ebiten.Image
 var arrow *ebiten.Image
 var img *ebiten.Image //variable declared for pointer image
 
-var arrowPos = [2]int{11, 12}
+var arrowPos = [2]int{0, 0}
 var (
 	mplusNormalFont font.Face
 	mplusBigFont    font.Face
@@ -59,7 +65,10 @@ func init() { //init function grabbing image from directory
 
 	player.Name = "Blue Gopher"
 	player.Actions = [4]Action{spell1, spell2, spell3, spell4}
-	player.Health = [2]int{25, 25}
+	player.Stats.maxHealth = 25
+	player.Stats.currentHealth = 25
+	player.Stats.attack = 0
+	player.Stats.defense = 0
 	player.Image = *img
 	var x, y = player.Image.Size()
 	player.Size = [2]int{x, y}
@@ -67,21 +76,13 @@ func init() { //init function grabbing image from directory
 
 	enemy.Name = "Red Gopher"
 	enemy.Actions = [4]Action{spell5, spell6, spell7, spell8}
-	enemy.Health = [2]int{10, 10}
+	enemy.Stats.maxHealth = 25
+	enemy.Stats.currentHealth = 25
+	enemy.Stats.attack = 0
+	enemy.Stats.defense = 0
 	enemy.Image = *img
 	enemy.Size = [2]int{x, y}
 	enemy.Position = [2]int{windowWidth * 19 / 20, windowHeight * 2 / 10}
-
-	/*
-		fmt.Println("-Player Position-")
-		fmt.Println(player.Position)
-		fmt.Println("-Player Size-")
-		fmt.Println(player.Size)
-		fmt.Println("-Enemy Position-")
-		fmt.Println(enemy.Position)
-		fmt.Println("-Enemy Size-")
-		fmt.Println(enemy.Size)
-		fmt.Println(img.Bounds())*/
 
 	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
 	if err != nil {
@@ -111,7 +112,7 @@ func init() { //init function grabbing image from directory
 type Action struct {
 	Name           string
 	HealthModifier int //How much it modifies health + or -
-	CoolDown       int
+	CoolDown       [2]int
 	Effect         string
 }
 
@@ -119,9 +120,81 @@ type Entity struct {
 	Name     string
 	Position [2]int
 	Size     [2]int
-	Health   [2]int //{Current Health, Total Health}
-	Actions  [4]Action
-	Image    ebiten.Image
+	Stats    struct {
+		maxHealth     int
+		currentHealth int
+		attack        int
+		defense       int
+	}
+	Actions [4]Action
+	Image   ebiten.Image
+}
+
+func ActionEffects(first Entity, second Entity, spell Action) (struct {
+	maxHealth     int
+	currentHealth int
+	attack        int
+	defense       int
+}, struct {
+	maxHealth     int
+	currentHealth int
+	attack        int
+	defense       int
+}, [4]Action,
+	bool) {
+	for i := 0; i < len(first.Actions); i++ {
+		if first.Actions[i].CoolDown[1] < turn {
+			if first.Actions[i] == spell {
+				if spell.Effect == "attackBuff" {
+					first.Stats.attack = 0
+				} else if spell.Effect == "defenseBuff" {
+					first.Stats.defense = 0
+				}
+				first.Actions[i].CoolDown[1] += spell.CoolDown[0]
+				fmt.Println("cooldown increased")
+			}
+			first.Actions[i].CoolDown[1] += 1
+		}
+		if first.Actions[i].CoolDown[1] == turn {
+			if first.Actions[i].Effect == "attackBuff" && first.Stats.attack > 0 {
+				first.Stats.attack = 0
+				fmt.Println("attack buff wore off")
+				fmt.Println("player attack", first.Stats.attack)
+			} else if first.Actions[i].Effect == "defenseBuff" && first.Stats.defense > 0 {
+				first.Stats.defense = 0
+				fmt.Println("defense buff wore off")
+				fmt.Println("player defense", first.Stats.defense)
+			}
+		}
+	}
+	var dead bool = false
+	if spell.Effect == "damage" {
+		second.Stats.currentHealth -= (spell.HealthModifier + first.Stats.attack - second.Stats.defense)
+		spell.CoolDown[1] += spell.CoolDown[0]
+		if first.Stats.currentHealth < 1 {
+			first.Stats.currentHealth = 0
+			dead = true
+		}
+		//fmt.Println(first.Name)
+		//fmt.Println("used Damage spell")
+	} else if spell.Effect == "heal" {
+		first.Stats.currentHealth += spell.HealthModifier
+		if first.Stats.currentHealth > first.Stats.maxHealth {
+			first.Stats.currentHealth = first.Stats.maxHealth
+		}
+		//fmt.Println(first.Name)
+		//fmt.Println("used heal spell")
+	} else if spell.Effect == "attackBuff" {
+		first.Stats.attack += spell.HealthModifier
+		//fmt.Println(first.Name)
+		//fmt.Println("used attack buff spell")
+	} else if spell.Effect == "defenseBuff" {
+		first.Stats.defense += spell.HealthModifier
+		//fmt.Println(first.Name)
+		//fmt.Println("used defense buff spell")
+	}
+
+	return first.Stats, second.Stats, first.Actions, dead
 }
 
 type Game struct {
@@ -150,16 +223,95 @@ func (g *Game) Update() error {
 	if inpututil.IsKeyJustPressed(ebiten.KeyRight) {
 		arrowPos[0] = 11
 	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
+	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) && playerTurn {
 		if arrowPos == [2]int{0, 0} {
-			enemy.Health[0] -= player.Actions[0].HealthModifier
+			fmt.Println("current turn:", turn)
+			fmt.Println("current cd:", player.Actions[0].CoolDown[1])
+
+			if player.Actions[0].CoolDown[1] == turn {
+				playerTurn = false
+				turn += 1
+				player.Stats, enemy.Stats, player.Actions, enemyDead = ActionEffects(player, enemy, player.Actions[0])
+				turnText = "Enemy's Turn"
+				time.AfterFunc(DurationOfTime, func() {
+					var x int
+					for {
+						x = rand.Intn(4)
+						if (enemy.Actions[x].CoolDown[1] + 1) == turn {
+							break
+						}
+					}
+
+					enemy.Stats, player.Stats, enemy.Actions, playerDead = ActionEffects(enemy, player, enemy.Actions[x])
+					turnText = "Your Turn"
+					playerTurn = true
+					//fmt.Println(player.Stats)
+					//fmt.Println(enemy.Stats)
+				})
+			} else {
+				turnText = "That spell is on COOLDOWN!"
+			}
 		} else if arrowPos == [2]int{11, 0} {
-			enemy.Health[0] -= player.Actions[1].HealthModifier
+			fmt.Println("current turn:", turn)
+			fmt.Println("current cd:", player.Actions[1].CoolDown[1])
+			if player.Actions[1].CoolDown[1] == turn {
+				playerTurn = false
+				turn += 1
+				player.Stats, enemy.Stats, player.Actions, enemyDead = ActionEffects(player, enemy, player.Actions[1])
+				turnText = "Enemy's Turn"
+				time.AfterFunc(DurationOfTime, func() {
+					x := rand.Intn(4)
+					enemy.Stats, player.Stats, enemy.Actions, playerDead = ActionEffects(enemy, player, enemy.Actions[x])
+					turnText = "Your Turn"
+					playerTurn = true
+					//fmt.Println(player.Stats)
+					//fmt.Println(enemy.Stats)
+				})
+			} else {
+				turnText = "That spell is on COOLDOWN!"
+			}
 		} else if arrowPos == [2]int{0, 12} {
-			enemy.Health[0] -= player.Actions[2].HealthModifier
+			fmt.Println("current turn:", turn)
+			fmt.Println("current cd:", player.Actions[2].CoolDown[1])
+			if player.Actions[2].CoolDown[1] == turn {
+				playerTurn = false
+				turn += 1
+				player.Stats, enemy.Stats, player.Actions, enemyDead = ActionEffects(player, enemy, player.Actions[2])
+				turnText = "Enemy's Turn"
+				time.AfterFunc(DurationOfTime, func() {
+					x := rand.Intn(4)
+					enemy.Stats, player.Stats, enemy.Actions, playerDead = ActionEffects(enemy, player, enemy.Actions[x])
+					turnText = "Your Turn"
+					playerTurn = true
+					//fmt.Println(player.Stats)
+					//fmt.Println(enemy.Stats)
+				})
+			} else {
+				turnText = "That spell is on COOLDOWN!"
+			}
 		} else if arrowPos == [2]int{11, 12} {
-			enemy.Health[0] -= player.Actions[3].HealthModifier
+			fmt.Println("current turn:", turn)
+			fmt.Println("current cd:", player.Actions[3].CoolDown[1])
+			if player.Actions[3].CoolDown[1] == turn {
+				playerTurn = false
+				turn += 1
+				player.Stats, enemy.Stats, player.Actions, enemyDead = ActionEffects(player, enemy, player.Actions[3])
+				turnText = "Enemy's Turn"
+				time.AfterFunc(DurationOfTime, func() {
+					x := rand.Intn(4)
+					enemy.Stats, player.Stats, enemy.Actions, playerDead = ActionEffects(enemy, player, enemy.Actions[x])
+					turnText = "Your Turn"
+					playerTurn = true
+					//fmt.Println(player.Stats)
+					//fmt.Println(enemy.Stats)
+				})
+			} else {
+				turnText = "That spell is on COOLDOWN!"
+			}
 		}
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+		player.Stats.currentHealth -= 1
 	}
 	return nil
 }
@@ -196,21 +348,21 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	hpEnemy.GeoM.Translate(float64(enemy.Position[0]-enemy.Size[0]*3/2+30), float64(enemy.Position[1]-40))
 	hpPlayer := &ebiten.DrawImageOptions{}
 	hpPlayer.GeoM.Translate(float64(player.Position[0]+30), float64(player.Position[1]-40))
-	healthBarGreenP = ebiten.NewImage(300*player.Health[0]/player.Health[1], 30)
+	healthBarGreenP = ebiten.NewImage(300*player.Stats.currentHealth/player.Stats.maxHealth, 30)
 	healthBarGreenP.Fill(color.RGBA{0x00, 0xff, 0x00, 0xff})
 	//fmt.Println(player.Health)
-	healthBarGreenE = ebiten.NewImage(300*enemy.Health[0]/enemy.Health[1], 30)
+	healthBarGreenE = ebiten.NewImage(300*enemy.Stats.currentHealth/enemy.Stats.maxHealth, 30)
 	healthBarGreenE.Fill(color.RGBA{0x00, 0xff, 0x00, 0xff})
 
 	healthBarRed = ebiten.NewImage(300, 30)
 	healthBarRed.Fill(color.RGBA{0xff, 0x00, 0x00, 0xff})
 
 	screen.DrawImage(healthBarRed, hpEnemy)
-	if !EnemyDead {
+	if !enemyDead {
 		screen.DrawImage(healthBarGreenE, hpEnemy)
 	}
 	screen.DrawImage(healthBarRed, hpPlayer)
-	if !PlayerDead {
+	if !playerDead {
 		screen.DrawImage(healthBarGreenP, hpPlayer)
 	}
 	screen.DrawImage(box, b)
@@ -218,10 +370,10 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.DrawImage(img, char)
 	screen.DrawImage(img, npc)
 
-	text.Draw(screen, "first spell", mplusNormalFont, windowWidth*12/40, windowHeight*57/80, color.White)
-	text.Draw(screen, "second spell", mplusNormalFont, windowWidth*(12+11)/40, windowHeight*57/80, color.White)
-	text.Draw(screen, "third spell", mplusNormalFont, windowWidth*12/40, windowHeight*(57+12)/80, color.White)
-	text.Draw(screen, "fourth spell", mplusNormalFont, windowWidth*(12+11)/40, windowHeight*(57+12)/80, color.White)
+	text.Draw(screen, player.Actions[0].Name, mplusNormalFont, windowWidth*12/40, windowHeight*57/80, color.White)
+	text.Draw(screen, player.Actions[1].Name, mplusNormalFont, windowWidth*(12+11)/40, windowHeight*57/80, color.White)
+	text.Draw(screen, player.Actions[2].Name, mplusNormalFont, windowWidth*12/40, windowHeight*(57+12)/80, color.White)
+	text.Draw(screen, player.Actions[3].Name, mplusNormalFont, windowWidth*(12+11)/40, windowHeight*(57+12)/80, color.White)
 
 	text.Draw(screen, turnText, mplusBigFont, windowWidth*15/40, windowHeight*1/20, color.White)
 
@@ -240,14 +392,3 @@ func main() {
 		log.Fatal(err)
 	}
 }
-
-/*
-SPELLS OPTIONS AND SELECTION
-	DRAW TEXT -DONE
-	POSITION TEXT - DONE
-	DRAW SELECTOR - DONE
-	ATTACH VARIABLE WITH VALUE FOR POSITION  0,0  0,1 1,0 1,1 -DONE
-	ATTACH VARIABLE CHANGE TO KEY PRESS DIRECTIONALLY - DONE
-ENEMY TURN
-BATTLE LOOP
-*/
