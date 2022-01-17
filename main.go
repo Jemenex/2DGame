d0cf00154a5d01ecff2f26787df6ef437f6a70d6
Jemenex <Jemenex@gmail.com>
2022-01-16 22:19:53 -0500

    update

--- main.go
+++ main.go
@@ -40,6 +40,7 @@ var enemy Entity
 
 var turnText string = "Battle Starts!"
 var turn int = 0
+var gameState string = "Battle" // Menu - Battle - Uppgrade after battle
 
 var healthBarGreenE *ebiten.Image
 var healthBarGreenP *ebiten.Image
@@ -54,12 +55,25 @@ var arrow *ebiten.Image
 var background *ebiten.Image
 
 //var img *ebiten.Image //variable declared for pointer image
+//go:embed HeroKnight.png
+var heroKnightPng []byte
 var hero *ebiten.Image
+
+//go:embed HeavyBandit.png
+var heavyBanditPng []byte
 var bandit *ebiten.Image
 
 const (
 	iconSize = 32
 	iconXNum = 16
+)
+
+const (
+	frameOX     = 100
+	frameOY     = 0
+	frameWidth  = 100
+	frameHeight = 55
+	frameNum    = 7
 )
 
 //go:embed icons.png
@@ -83,15 +97,27 @@ func init() { //init function grabbing image from directory
 	}
 	iconsImg = ebiten.NewImageFromImage(img)
 
+	img2, _, err := image.Decode(bytes.NewReader(heroKnightPng))
+	if err != nil {
+		log.Fatal(err)
+	}
+	hero = ebiten.NewImageFromImage(img2)
+
+	img3, _, err := image.Decode(bytes.NewReader(heavyBanditPng))
+	if err != nil {
+		log.Fatal(err)
+	}
+	bandit = ebiten.NewImageFromImage(img3)
+
 	background, _, err = ebitenutil.NewImageFromFile("background.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	hero, _, err = ebitenutil.NewImageFromFile("hero.png")
+	/*hero, _, err = ebitenutil.NewImageFromFile("hero.png")
 	if err != nil {
 		log.Fatal(err)
-	}
+	}*/
 
 	bandit, _, err = ebitenutil.NewImageFromFile("bandit.png")
 	if err != nil {
@@ -116,7 +142,8 @@ func init() { //init function grabbing image from directory
 	enemy.Stats.attack = 0
 	enemy.Stats.defense = 0
 	enemy.Image = *bandit
-	enemy.Size = [2]int{x, y}
+	var w, z = enemy.Image.Size()
+	enemy.Size = [2]int{w, z}
 	enemy.Position = [2]int{windowWidth*16/20 - enemy.Size[0], windowHeight * 4 / 10}
 
 	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
@@ -238,169 +265,153 @@ type Game struct {
 
 func (g *Game) Update() error {
 	g.count++
-	/*if inpututil.IsKeyJustPressed(ebiten.KeyUp) && player.Health[0] > 1 {
-		player.Health[0] -= 1
-	} else if inpututil.IsKeyJustPressed(ebiten.KeyUp) && player.Health[0] == 1 {
-		PlayerDead = true
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyDown) && enemy.Health[0] > 1 {
-		enemy.Health[0] -= 1
-	} else if inpututil.IsKeyJustPressed(ebiten.KeyDown) && enemy.Health[0] == 1 {
-		EnemyDead = true
-	}*/
-	if inpututil.IsKeyJustPressed(ebiten.KeyLeft) {
-		if arrowPos >= 64 {
-			arrowPos -= 64
+	if gameState == "Battle" { //Battle State Controls
+		if inpututil.IsKeyJustPressed(ebiten.KeyLeft) {
+			if arrowPos >= 64 {
+				arrowPos -= 64
+			}
 		}
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyRight) {
-		if arrowPos <= 384 {
-			arrowPos += 64
+		if inpututil.IsKeyJustPressed(ebiten.KeyRight) {
+			if arrowPos <= 384 {
+				arrowPos += 64
+			}
 		}
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) && playerTurn {
-		if arrowPos == 0 {
-			fmt.Println("current turn:", turn)
-			fmt.Println("current cd:", player.Actions[0].CoolDown[1])
+		if inpututil.IsKeyJustPressed(ebiten.KeyEnter) && playerTurn {
+			if arrowPos == 0 {
+				fmt.Println("current turn:", turn)
+				fmt.Println("current cd:", player.Actions[0].CoolDown[1])
 
-			if player.Actions[0].CoolDown[1] == turn {
-				playerTurn = false
-				turn += 1
-				player.Stats, enemy.Stats, player.Actions, enemyDead = ActionEffects(player, enemy, player.Actions[0])
-				turnText = "Enemy's Turn"
-				time.AfterFunc(DurationOfTime, func() {
-					var x int
-					for {
-						x = rand.Intn(4)
-						if (enemy.Actions[x].CoolDown[1] + 1) == turn {
-							break
+				if player.Actions[0].CoolDown[1] == turn {
+					playerTurn = false
+					turn += 1
+					player.Stats, enemy.Stats, player.Actions, enemyDead = ActionEffects(player, enemy, player.Actions[0])
+					turnText = "Enemy's Turn"
+					time.AfterFunc(DurationOfTime, func() {
+						var x int
+						for {
+							x = rand.Intn(4)
+							if (enemy.Actions[x].CoolDown[1] + 1) == turn {
+								break
+							}
 						}
-					}
 
-					enemy.Stats, player.Stats, enemy.Actions, playerDead = ActionEffects(enemy, player, enemy.Actions[x])
-					turnText = "Your Turn"
-					playerTurn = true
-					//fmt.Println(player.Stats)
-					//fmt.Println(enemy.Stats)
-				})
-			} else {
-				turnText = "That spell is on COOLDOWN!"
+						enemy.Stats, player.Stats, enemy.Actions, playerDead = ActionEffects(enemy, player, enemy.Actions[x])
+						turnText = "Your Turn"
+						playerTurn = true
+						//fmt.Println(player.Stats)
+						//fmt.Println(enemy.Stats)
+					})
+				} else {
+					turnText = "That spell is on COOLDOWN!"
+				}
+			} else if arrowPos == 64 {
+				fmt.Println("current turn:", turn)
+				fmt.Println("current cd:", player.Actions[1].CoolDown[1])
+				if player.Actions[1].CoolDown[1] == turn {
+					playerTurn = false
+					turn += 1
+					player.Stats, enemy.Stats, player.Actions, enemyDead = ActionEffects(player, enemy, player.Actions[1])
+					turnText = "Enemy's Turn"
+					time.AfterFunc(DurationOfTime, func() {
+						x := rand.Intn(4)
+						enemy.Stats, player.Stats, enemy.Actions, playerDead = ActionEffects(enemy, player, enemy.Actions[x])
+						turnText = "Your Turn"
+						playerTurn = true
+						//fmt.Println(player.Stats)
+						//fmt.Println(enemy.Stats)
+					})
+				} else {
+					turnText = "That spell is on COOLDOWN!"
+				}
+			} else if arrowPos == 128 {
+				fmt.Println("current turn:", turn)
+				fmt.Println("current cd:", player.Actions[2].CoolDown[1])
+				if player.Actions[2].CoolDown[1] == turn {
+					playerTurn = false
+					turn += 1
+					player.Stats, enemy.Stats, player.Actions, enemyDead = ActionEffects(player, enemy, player.Actions[2])
+					turnText = "Enemy's Turn"
+					time.AfterFunc(DurationOfTime, func() {
+						x := rand.Intn(4)
+						enemy.Stats, player.Stats, enemy.Actions, playerDead = ActionEffects(enemy, player, enemy.Actions[x])
+						turnText = "Your Turn"
+						playerTurn = true
+						//fmt.Println(player.Stats)
+						//fmt.Println(enemy.Stats)
+					})
+				} else {
+					turnText = "That spell is on COOLDOWN!"
+				}
+			} else if arrowPos == 192 {
+				fmt.Println("current turn:", turn)
+				fmt.Println("current cd:", player.Actions[3].CoolDown[1])
+				if player.Actions[3].CoolDown[1] == turn {
+					playerTurn = false
+					turn += 1
+					player.Stats, enemy.Stats, player.Actions, enemyDead = ActionEffects(player, enemy, player.Actions[3])
+					turnText = "Enemy's Turn"
+					time.AfterFunc(DurationOfTime, func() {
+						x := rand.Intn(4)
+						enemy.Stats, player.Stats, enemy.Actions, playerDead = ActionEffects(enemy, player, enemy.Actions[x])
+						turnText = "Your Turn"
+						playerTurn = true
+						//fmt.Println(player.Stats)
+						//fmt.Println(enemy.Stats)
+					})
+				} else {
+					turnText = "That spell is on COOLDOWN!"
+				}
+			} else if arrowPos > 192 {
+				turnText = "No spell assigned!"
 			}
-		} else if arrowPos == 64 {
-			fmt.Println("current turn:", turn)
-			fmt.Println("current cd:", player.Actions[1].CoolDown[1])
-			if player.Actions[1].CoolDown[1] == turn {
-				playerTurn = false
-				turn += 1
-				player.Stats, enemy.Stats, player.Actions, enemyDead = ActionEffects(player, enemy, player.Actions[1])
-				turnText = "Enemy's Turn"
-				time.AfterFunc(DurationOfTime, func() {
-					x := rand.Intn(4)
-					enemy.Stats, player.Stats, enemy.Actions, playerDead = ActionEffects(enemy, player, enemy.Actions[x])
-					turnText = "Your Turn"
-					playerTurn = true
-					//fmt.Println(player.Stats)
-					//fmt.Println(enemy.Stats)
-				})
-			} else {
-				turnText = "That spell is on COOLDOWN!"
-			}
-		} else if arrowPos == 128 {
-			fmt.Println("current turn:", turn)
-			fmt.Println("current cd:", player.Actions[2].CoolDown[1])
-			if player.Actions[2].CoolDown[1] == turn {
-				playerTurn = false
-				turn += 1
-				player.Stats, enemy.Stats, player.Actions, enemyDead = ActionEffects(player, enemy, player.Actions[2])
-				turnText = "Enemy's Turn"
-				time.AfterFunc(DurationOfTime, func() {
-					x := rand.Intn(4)
-					enemy.Stats, player.Stats, enemy.Actions, playerDead = ActionEffects(enemy, player, enemy.Actions[x])
-					turnText = "Your Turn"
-					playerTurn = true
-					//fmt.Println(player.Stats)
-					//fmt.Println(enemy.Stats)
-				})
-			} else {
-				turnText = "That spell is on COOLDOWN!"
-			}
-		} else if arrowPos == 192 {
-			fmt.Println("current turn:", turn)
-			fmt.Println("current cd:", player.Actions[3].CoolDown[1])
-			if player.Actions[3].CoolDown[1] == turn {
-				playerTurn = false
-				turn += 1
-				player.Stats, enemy.Stats, player.Actions, enemyDead = ActionEffects(player, enemy, player.Actions[3])
-				turnText = "Enemy's Turn"
-				time.AfterFunc(DurationOfTime, func() {
-					x := rand.Intn(4)
-					enemy.Stats, player.Stats, enemy.Actions, playerDead = ActionEffects(enemy, player, enemy.Actions[x])
-					turnText = "Your Turn"
-					playerTurn = true
-					//fmt.Println(player.Stats)
-					//fmt.Println(enemy.Stats)
-				})
-			} else {
-				turnText = "That spell is on COOLDOWN!"
-			}
-		} else if arrowPos > 192 {
-			turnText = "No spell assigned!"
 		}
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
-		player.Stats.currentHealth -= 1
-	}
-	if playerDead {
-		turnText = "Battle is over " + player.Name + " has died"
-	} else if enemyDead {
-		turnText = "Battle is over " + enemy.Name + " has died"
+		if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+			player.Stats.currentHealth -= 1
+		}
+		if playerDead {
+			turnText = "Battle is over " + player.Name + " has died"
+		} else if enemyDead {
+			turnText = "Battle is over " + enemy.Name + " has died"
+		}
 	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Capture Mouse Position in variables
-	x, y := ebiten.CursorPosition()
-	// Display Mouse Position
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("X: %d, Y: %d", x, y))
-
+	//Enemy Draw Options - Position . Scale
 	npc := &ebiten.DrawImageOptions{}
 	npc.GeoM.Scale(4.5, 4.5)
 	npc.GeoM.Translate(float64(enemy.Position[0]), float64(enemy.Position[1]))
-
-	//fmt.Println(npc.GeoM)
-
+	//Player Draw Options - Position . Scale
 	char := &ebiten.DrawImageOptions{}
 	char.GeoM.Scale(4, 4)
 	char.GeoM.Translate(float64(player.Position[0]), float64(player.Position[1]))
-
-	b := &ebiten.DrawImageOptions{}
-	b.GeoM.Translate(float64(windowWidth)*4/20, float64(windowHeight)*25/40)
-
+	//Player
+	ap := &ebiten.DrawImageOptions{}
+	ap.GeoM.Scale(4, 4)
+	ap.GeoM.Translate(float64(player.Position[0]), float64(player.Position[1]))
+	//Background Draw Options - Scale
 	bg := &ebiten.DrawImageOptions{}
 	bg.GeoM.Scale(1.3, 1.3)
-	//box.Fill(color.RGBA{0x00, 0x00, 0x00, 0x7f})
-
+	//Arrow/Highlight Draw Options - Position . Size . Color
 	arr := &ebiten.DrawImageOptions{}
 	arr.GeoM.Translate(float64(384+arrowPos), float64(832))
 	arrow = ebiten.NewImage(64, 64)
 	arrow.Fill(color.RGBA{0xf9, 0xe8, 0x2f, 0x2f})
-
+	//Hp Bar Draw Options - Position . Size . Fill
 	hpEnemy := &ebiten.DrawImageOptions{}
 	hpEnemy.GeoM.Translate(float64(enemy.Position[0]-enemy.Size[0]+40), float64(enemy.Position[1]-40))
 	hpPlayer := &ebiten.DrawImageOptions{}
 	hpPlayer.GeoM.Translate(float64(player.Position[0]+40), float64(player.Position[1]-40))
 	healthBarGreenP = ebiten.NewImage(300*player.Stats.currentHealth/player.Stats.maxHealth, 15)
 	healthBarGreenP.Fill(color.RGBA{0x00, 0xff, 0x00, 0xff})
-	//fmt.Println(player.Health)
 	healthBarGreenE = ebiten.NewImage(300*enemy.Stats.currentHealth/enemy.Stats.maxHealth, 15)
 	healthBarGreenE.Fill(color.RGBA{0x00, 0xff, 0x00, 0xff})
-
 	healthBarRed = ebiten.NewImage(300, 15)
 	healthBarRed.Fill(color.RGBA{0xff, 0x00, 0x00, 0xff})
-
+	//Background DrawImage function
 	screen.DrawImage(background, bg)
-
+	//HP bar DrawImage function and conditionals
 	screen.DrawImage(healthBarRed, hpEnemy)
 	if !enemyDead {
 		screen.DrawImage(healthBarGreenE, hpEnemy)
@@ -409,17 +420,12 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	if !playerDead {
 		screen.DrawImage(healthBarGreenP, hpPlayer)
 	}
-	//screen.DrawImage(box, b)
-	screen.DrawImage(hero, char)
+	//Player and Enemy DrawImage function
+	//screen.DrawImage(hero, char)
 	screen.DrawImage(bandit, npc)
-
-	/*text.Draw(screen, player.Actions[0].Name, mplusNormalFont, windowWidth*12/40, windowHeight*57/80, color.White)
-	text.Draw(screen, player.Actions[1].Name, mplusNormalFont, windowWidth*(12+11)/40, windowHeight*57/80, color.White)
-	text.Draw(screen, player.Actions[2].Name, mplusNormalFont, windowWidth*12/40, windowHeight*(57+12)/80, color.White)
-	text.Draw(screen, player.Actions[3].Name, mplusNormalFont, windowWidth*(12+11)/40, windowHeight*(57+12)/80, color.White)*/
-
+	//Turn Text DrawImage function
 	text.Draw(screen, turnText, mplusBigFont, windowWidth/2-(text.BoundString(mplusBigFont, turnText).Dx()/2), windowHeight*1/20, color.White)
-
+	//Array iteration to position a tileset using subimages and positions - Used for hotbar
 	const xNum = (windowWidth / 2) / iconSize
 	for _, l := range g.layers {
 		for i, t := range l {
@@ -433,13 +439,17 @@ func (g *Game) Draw(screen *ebiten.Image) {
 			screen.DrawImage(iconsImg.SubImage(image.Rect(sx, sy, sx+iconSize, sy+iconSize)).(*ebiten.Image), op)
 		}
 	}
+	//Arrow DrawImage function and hotkey Text drawn
 	screen.DrawImage(arrow, arr)
 	for i := 0; i < 8; i++ {
 		text.Draw(screen, strconv.Itoa(i+1), mplusNormalFont, 428+(i*64), 890, color.White)
 	}
-
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS()))
-
+	// Capture Mouse Position in variables
+	x, y := ebiten.CursorPosition()
+	//FPS shown top
+	// Display Mouse Position
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f   X: %d, Y: %d", ebiten.CurrentTPS(), x, y))
+	//Fade in box drawn and alpha change
 	box = ebiten.NewImage(windowWidth, windowHeight)
 	box.Fill(color.RGBA{0, 0, 0, fadeAlpha})
 	screen.DrawImage(box, nil) //test for fade in from black transparency
@@ -449,6 +459,11 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	} else if fadeAlpha < 10 {
 		fadeAlpha = 0
 	}
+
+	i := (g.count / 6) % frameNum
+	sx, sy := frameOX+i*frameWidth, frameOY
+	screen.DrawImage(hero.SubImage(image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)).(*ebiten.Image), ap)
+
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
